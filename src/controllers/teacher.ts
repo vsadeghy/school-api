6a7f62392377ed18b2396e3d34479af64595e7f2
vahid sadeghy <v.sadeghy@gmail.com>
2021-04-14 03:42:28 +0430

    added set student grade

--- src/controllers/teacher.ts
+++ src/controllers/teacher.ts
@@ -1,13 +1,16 @@
 import argon2 from "argon2";
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
+import Lesson from "../models/lesson";
+import Student from "../models/student";
 import Teacher from "../models/teacher";
+import { ID } from "../types";
 import { UserInput } from "../utils/UserInput";
 
 export const getTeachers = async (_req: Request, res: Response) => {
-    const teachers = await Teacher.find();
-    // .populate("lessons", "-__v -teacher")
-    // .select("-__v -password");
+    const teachers = await Teacher.find()
+        .populate("lessons", "-teacher -__v")
+        .select("-__v -password");
     if (!teachers.length) {
         res.send("no teachers found");
         return;
@@ -16,7 +19,6 @@ export const getTeachers = async (_req: Request, res: Response) => {
 };
 
 export const createTeacher = async (req: Request, res: Response) => {
-    console.log("checking for existing teacher");
     const body: UserInput = req.body;
     const existingTeacherWithMail = await Teacher.findByEmail(body.email);
     const existingTeacherWithUsername = await Teacher.findByUsername(
@@ -28,14 +30,12 @@ export const createTeacher = async (req: Request, res: Response) => {
         );
     }
     const hashedPassword = await argon2.hash(body.password);
-    console.log("creating teacher");
     const teacher = await Teacher.create({
         name: body.name,
         email: body.email,
         username: body.username,
         password: hashedPassword,
     });
-    console.log("teacher created: ", teacher);
     const teacherId = teacher._id.toString();
     const token = jwt.sign(
         {
@@ -45,11 +45,60 @@ export const createTeacher = async (req: Request, res: Response) => {
         process.env.JWT_SECRET,
         { expiresIn: "1h" }
     );
-    res.json({ token, teacherId });
+    res.json({ token, teacher });
 };
 
 export const deleteTeacher = async (req: Request, res: Response) => {
     const { id } = req.params;
     await Teacher.findByIdAndDelete(id);
     res.send("teacher deleted");
+};
+
+export const setStudentGrade = async (req: Request, res: Response) => {
+    const lessonId = (req.params.lessonId as unknown) as ID;
+    const id = (req.params.id as unknown) as ID;
+    const grade = req.body.grade as number;
+    const studentId = req.body.studentId as ID;
+    if (!grade) {
+        res.status(400).send("send a grade");
+        return;
+    }
+    if (grade > 20 || grade < 0) {
+        res.status(400).send("grade should be between 0 and 20");
+        return;
+    }
+
+    if (!studentId) {
+        res.status(400).send("send id of the student");
+        return;
+    }
+
+    const teacher = await Teacher.findById(id);
+    if (!teacher) {
+        res.status(404).send("teacher not found");
+        return;
+    }
+
+    if (!(teacher.lessons as ID[]).includes(lessonId)) {
+        res.send("only the lesson's teacher can set grades");
+        return;
+    }
+
+    const lesson = await Lesson.findById(lessonId);
+    if (!lesson) {
+        res.status(404).send("lesson not found");
+        return;
+    }
+    if (!(lesson.students as ID[]).includes(studentId)) {
+        res.status(404).send("teacher does not have this student");
+        return;
+    }
+    const student = await Student.findById(studentId);
+    if (!student) {
+        res.status(404).send("student not found");
+        return;
+    }
+    student.getLesson(lessonId).grade = grade;
+    await student.save();
+    res.send("student grade set");
 };
--- src/routes/teacher.ts
+++ src/routes/teacher.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
-import { deleteTeacher, getTeachers } from "../controllers/teacher";
+import {
+    deleteTeacher,
+    getTeachers,
+    setStudentGrade,
+} from "../controllers/teacher";
 import { validUser } from "../middlewares/validUser";
 import { createTeacher } from "./../controllers/teacher";
 
@@ -7,6 +11,7 @@ const router = Router();
 
 router.get("/", getTeachers);
 router.post("/signup", validUser, createTeacher);
-router.delete(":id", deleteTeacher);
+router.delete("/:id", deleteTeacher);
+router.put("/:id/lessons/:lessonId/setStudentGrade", setStudentGrade);
 
 export const teacherRouter = router;
